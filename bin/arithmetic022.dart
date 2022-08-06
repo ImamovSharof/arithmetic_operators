81f789ae4ab561255948cf378016836902e617ee
sharof <sharofimamov3@gmail.com>
2022-08-07 00:28:33 +0500

    1

--- bin/arithmetic022.dart
+++ bin/arithmetic022.dart
@@ -1,4 +1,12 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+
+double func(int a, int b, int c) {
+  return (2 * pow(b, 2) - a) / pow(a, 3) - pow(c, 2);
+}
+
+void main() {
+  print(func(1, 11, 3));
+}
--- bin/arithmetic025.dart
+++ bin/arithmetic025.dart
@@ -1,4 +1,12 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+
+double func(int a, int b) {
+  return 2 * a * (a + b) / (4 * a);
+}
+
+void main() {
+  print(func(2, 4));
+}
